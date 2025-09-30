f1d71d261a26d445619be48069b4b2d475ceea92
nollenr <nollenr@gmail.com>
2025-09-30 12:33:29 -0700

    reverting some output changes required for multi-region

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-# output "join_string" {
-#   description = "the CockroachDB private IP join string"
-#   value = local.join_string
-# }
+output "join_string" {
+  description = "the CockroachDB private IP join string"
+  value = local.join_string
+}
 
 # output "join_string_public" {
 #   description = "the CockroachDB public IP join string"
@@ -35,61 +35,61 @@
 #   sensitive = true
 # }
 
-# output "subnets" {
-#   description = "Subnets"
-#   value = local.subnet_list[*]
-# }
+output "subnets" {
+  description = "Subnets"
+  value = local.subnet_list[*]
+}
 
-# output "private_subnet_list" {
-#   description = "private subnets"
-#   value = local.private_subnet_list
-# }
+output "private_subnet_list" {
+  description = "private subnets"
+  value = local.private_subnet_list
+}
 
-# output "public_subnet_list" {
-#   description = "public subnets"
-#   value = local.public_subnet_list
-# }
+output "public_subnet_list" {
+  description = "public subnets"
+  value = local.public_subnet_list
+}
 
-# output "availability_zones" {
-#   description = "availability zones"
-#   value = data.aws_availability_zones.available.names
-# }
+output "availability_zones" {
+  description = "availability zones"
+  value = data.aws_availability_zones.available.names
+}
 
-# output "availability_zone_list" {
-#   description = "availability zone list"
-#   value = local.availability_zone_list
-# }
+output "availability_zone_list" {
+  description = "availability zone list"
+  value = local.availability_zone_list
+}
 
-# output "network_interfaces" {
-#   description = "List of network interfaces"
-#   value       = aws_network_interface.crdb[*].private_ip
-# }
+output "network_interfaces" {
+  description = "List of network interfaces"
+  value       = aws_network_interface.crdb[*].private_ip
+}
 
 
-# output "vpc_id" {
-#   description = "ID of the VPC created by the module"
-#   value       = aws_vpc.main.id
-# }
+output "vpc_id" {
+  description = "ID of the VPC created by the module"
+  value       = aws_vpc.main.id
+}
 
-# output "route_table_public_id" {
-#   description = "ID of the public route table"
-#   value = aws_route_table.public_route_table.id
-# }
+output "route_table_public_id" {
+  description = "ID of the public route table"
+  value = aws_route_table.public_route_table.id
+}
 
-# output "route_table_private_id" {
-#   description = "ID of the private route table"
-#   value = aws_route_table.private_route_table.id
-# }
+output "route_table_private_id" {
+  description = "ID of the private route table"
+  value = aws_route_table.private_route_table.id
+}
 
-# output "security_group_intra_node_id" {
-#   description = "ID of the security group allowing intra-node communication"
-#   value = module.security-group-02.security_group_id
-# }
+output "security_group_intra_node_id" {
+  description = "ID of the security group allowing intra-node communication"
+  value = module.security-group-02.security_group_id
+}
 
-# output "security_group_external_access_id" {
-#   description = "ID of the security group allowing communication external to the VPC"
-#   value = module.security-group-01.security_group_id
-# }
+output "security_group_external_access_id" {
+  description = "ID of the security group allowing communication external to the VPC"
+  value = module.security-group-01.security_group_id
+}
 
 # output "haproxy_ip" {
 #   description = "HA Proxy Private IP"
